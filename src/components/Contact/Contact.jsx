e9dbeb143fb7e28aba336b3e627225735313a289
Vinay <155900764+VinayWebDev05@users.noreply.github.com>
2024-05-17 00:16:32 +0530

    Add files via upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:vinaykumarj320@gmail.com">vinaykumarj320@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="www.linkedin.com/in/vinay-jangra-74063a256">www.linkedin.com/in/vinay-jangra-74063a256</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/VinayWebDev05">https://www.github.com/VinayWebDev05</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
